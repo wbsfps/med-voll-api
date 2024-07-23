7c327577556202f4e9872e48d6fc6417165a5dad
William Santana <williamandrade1058@gmail.com>
2024-07-23 18:05:14 -0300

    entidades JPA

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,28 @@
+package med.voll.api.medico;
+
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity(name = "medicos")
+@Table(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+}
