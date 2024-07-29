eb595f02c209b8361dfe582e42de047e3a5c8130
William Santana <williamandrade1058@gmail.com>
2024-07-28 22:54:12 -0300

    tratando erro 400

--- src/main/java/med/voll/api/infra/TratamentoDeErros.java
+++ src/main/java/med/voll/api/infra/TratamentoDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,4 +15,16 @@ public class TratamentoDeErros {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratando400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String nome, String mensagem) {
+        public DadosErroValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
