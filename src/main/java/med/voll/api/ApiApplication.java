ade49dbd9222a6896f1ef1f127634bcbf844e1fa
William Santana <williamandrade1058@gmail.com>
2024-09-09 15:34:19 -0300

    modificando SecurityConfigurations

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -1,6 +1,5 @@
 package med.voll.api;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
