6ff913a2e21bd08c22af8fca7264b6ef461611da
William Santana <williamandrade1058@gmail.com>
2024-09-18 22:45:14 -0300

    modificando m.ativo para true

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -14,7 +14,8 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     @Query("""
             select m from Medico m
-            where m.ativo = 1
+            where
+            m.ativo = true
             and
             m.especialidade = :especialidade
             and
@@ -22,7 +23,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
                 select c.medico.id from Consulta c
                 where
                 c.data = :data
-                )
+            )
             order by rand()
             limit 1
             """)
