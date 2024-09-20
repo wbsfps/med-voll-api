d691f162348527d57bfdcd164239501a11a0ef33
William Santana <williamandrade1058@gmail.com>
2024-09-20 11:11:05 -0300

    aplicando solid

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -3,12 +3,16 @@ package med.voll.api.domain.consulta.validacoes;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorMedicoAtivo {
+@Component
+public class ValidadorMedicoAtivo implements ValidadorAgendamentoDeConsulta {
 
+    @Autowired
     private MedicoRepository repository;
 
-    public void validador(DadosAgendamentoConsulta dados) {
+    public void validar(DadosAgendamentoConsulta dados) {
         var medico = dados.idMedico();
 
         if (medico == null) {
